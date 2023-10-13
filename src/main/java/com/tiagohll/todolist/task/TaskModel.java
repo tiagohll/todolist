3a2be690290d9ad088018ab318d3200710354696
Tiago <tiagoff2012@gmail.com>
2023-10-13 20:21:32 -0300

    Dockerfile deploy

---
+++ src/main/java/com/tiagohll/todolist/task/TaskModel.java
@@ -0,0 +1,52 @@
+package com.tiagohll.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+    /*
+     * 
+     * ID (USER_ID)
+     * User
+     * Description
+     * Tiitle
+     * Start date
+     * End date
+     * Priority
+     * 
+     */
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50) {
+            throw new Exception("Title must be less than 50 characters");
+        }
+        this.title = title;
+    }
+
+}
